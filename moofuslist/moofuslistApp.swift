f6117f14cbfc48e96ab6fd2dd2966d1961c3ec5e
Lamar Williams <lamar@moofus.com>
2025-04-18 16:08:29 -0700

    Initial Commit

---
+++ moofuslist/moofuslistApp.swift
@@ -0,0 +1,17 @@
+//
+//  moofuslistApp.swift
+//  moofuslist
+//
+//  Created by Lamar Williams III on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct moofuslistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
