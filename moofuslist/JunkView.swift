e88ee3104bde8c1a14d566ff9d95e2ca81e527aa
Lamar Williams <lamar@moofus.com>
2025-12-25 11:04:17 -0800

    progress, location services are working. removed unused location sample code

---
+++ moofuslist/JunkView.swift
@@ -0,0 +1,18 @@
+//
+//  JunkView.swift
+//  moofuslist
+//
+//  Created by Lamar Williams III on 12/24/25.
+//
+
+import SwiftUI
+
+struct JunkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JunkView()
+}
---
+++ moofuslist/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  moofuslist
+//
+//  Created by Lamar Williams III on 12/24/25.
+//
+
+import Foundation
