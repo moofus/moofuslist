c7107e1e58cf839f042c7aad609bd5bda60d7577
Lamar Williams <lamar@moofus.com>
2025-12-27 20:53:45 -0800

    progress

--- moofuslist/Views/MoofuslistView/MoofuslistSource.swift
+++ moofuslist/Views/MoofuslistView/MoofuslistSource.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FactoryKit
+import MapKit
 
 final actor MoofuslistSource {
   @Injected(\.locationManager) var locationManager: LocationManager
@@ -21,6 +22,24 @@ final actor MoofuslistSource {
   private func handleLocationManager() async {
     for await location in locationManager.stream {
       print(location)
+      // Reverse geocode with MapKit
+
+      if let request = MKReverseGeocodingRequest(location: location) {
+          do {
+              let mapItems = try await request.mapItems
+              print("items=")
+            for item in mapItems {
+              print(item)
+              print("city=\(item.addressRepresentations?.cityName ?? "home")")
+              print("cityWithContext=\(item.addressRepresentations?.cityWithContext)")
+              print("regionName=\(item.addressRepresentations?.regionName)")
+              print("region=\(item.addressRepresentations?.region)")
+            }
+            print("items end")
+          } catch {
+              print("Error reverse geocoding location: \(error)")
+          }
+      }
     }
   }
 }
@@ -32,3 +51,17 @@ extension MoofuslistSource {
     print("sleeping end")
   }
 }
+/*
+ <MKMapItem: 0x116771f40> {
+     address = "6825 Elverton Dr, Oakland, CA  94611, United States";
+     isCurrentLocation = 0;
+     name = "6825 Elverton Dr";
+     placemark = "6825 Elverton Dr, 6825 Elverton Dr, Oakland, CA  94611, United States @ <+37.84662530,-122.20234210> +/- 0.00m, region CLCircularRegion (identifier:'<+37.84662530,-122.20234210> radius 70.59', center:<+37.84662530,-122.20234210>, radius:70.59m)";
+     timeZone = "America/Los_Angeles (PST) offset -28800";
+ }
+ item.city=Oakland
+ item.cityWithContext=Optional("Oakland, CA")
+ item.regionName=Optional("United States")
+ item.region=Optional(US)
+
+ */
